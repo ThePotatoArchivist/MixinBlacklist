0ba124b7efccdba183fd2a5098c8dd58ba6d1353
JosiahFu <josiahfu@gmail.com>
2025-05-03 04:12:34 -0700

    qualify

--- src/main/java/archives/tater/mixinblacklist/MixinBlacklistCanceller.java
+++ src/main/java/archives/tater/mixinblacklist/MixinBlacklistCanceller.java
@@ -6,14 +6,12 @@ import net.fabricmc.loader.api.FabricLoader;
 
 import java.util.List;
 
-import static archives.tater.mixinblacklist.MixinBlacklist.ENTRIES;
-
 public class MixinBlacklistCanceller implements MixinCanceller {
 
     @Override
     public boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
         var isClient = FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT;
-        for (var entry : ENTRIES) {
+        for (var entry : MixinBlacklist.ENTRIES) {
             if (!isClient && entry.isClient) continue;
             if (entry.isTarget ? !targetClassNames.contains(entry.className) : !entry.className.equals(mixinClassName)) continue;
             entry.setApplied();
